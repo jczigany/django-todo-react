723bb671254dd858297b78655e9ba7cfbc288fba
Czigany Janos <jczigany@infomagus.hu>
2023-06-26 17:40:22 +0200

    update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import routers
 from todo import views
 
+
 router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo')
 
